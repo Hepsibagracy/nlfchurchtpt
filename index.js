1a0f058718c8474c3bb277483323bb0d95af2772
Hepsiba Gracy <hepsibagracy10@gmail.com>
2024-01-05 10:15:25 +0530

    updated

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function func()
-{
-    
-   }
+function myFunc(){
+   var x=document.getElementById("myTopnav");
+   if (x.className === "topnav"){
+       x.className += "responsive";
+   }else[
+       x.className = "topnav"
+   ]
+  }
+
